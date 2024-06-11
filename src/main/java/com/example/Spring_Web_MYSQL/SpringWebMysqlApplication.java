102a58a6f3a2be9bbb83862e496591d6d4d4ced4
Yosef-Perelman <yosefper@gmail.com>
2024-06-11 13:04:15 +0300

    Initial commit

---
+++ src/main/java/com/example/Spring_Web_MYSQL/SpringWebMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring_Web_MYSQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebMysqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Spring_Web_MYSQL/SpringWebMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring_Web_MYSQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
